137a3ea78d657a4bc8e7988e57ba14ae34e1a854
Ad3l31n3 <adomel555@gmail.com>
2024-05-27 22:55:57 -0400

    help

---
+++ diste/data/mods/copedraft/pokedex.js
@@ -0,0 +1,117 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var pokedex_exports = {};
+__export(pokedex_exports, {
+  Pokedex: () => Pokedex
+});
+module.exports = __toCommonJS(pokedex_exports);
+const Pokedex = {
+  ribbizap: {
+    inherit: true,
+    evos: ["Bulbfrog"]
+  },
+  bulbfrog: {
+    inherit: true,
+    prevo: "Ribbizap",
+    evoType: "useItem",
+    evoItem: "Thunder Stone"
+  },
+  anaconduke: {
+    inherit: true,
+    evos: ["Ballboa"]
+  },
+  ballboa: {
+    inherit: true,
+    prevo: "Anaconduke",
+    evoLevel: 50
+  },
+  honrade: {
+    inherit: true,
+    evos: ["Devante", "Vergilion"]
+  },
+  devante: {
+    inherit: true,
+    prevo: "Honrade",
+    evoType: "useItem",
+    evoItem: "Sun Stone"
+  },
+  vergilion: {
+    inherit: true,
+    prevo: "Honrade",
+    evoType: "useItem",
+    evoItem: "Moon Stone"
+  },
+  kuuroba: {
+    num: 69233,
+    name: "Kuuroba",
+    types: [
+      "Grass",
+      "Fighting"
+    ],
+    genderRatio: {
+      M: 0.25,
+      F: 0.75
+    },
+    baseStats: {
+      hp: 90,
+      atk: 105,
+      def: 90,
+      spa: 80,
+      spd: 90,
+      spe: 75
+    },
+    abilities: { 0: "Super Luck", H: "Super Luck", S: "Super Luck" },
+    heightm: 1.9,
+    weightkg: 102,
+    color: "Green",
+    prevo: "Clovour",
+    evoLevel: 60,
+    eggGroups: [
+      "Fairy",
+      "Grass"
+    ],
+    gen: 8
+  },
+  pantherk: {
+    inherit: true,
+    baseStats: {
+      hp: 85,
+      atk: 115,
+      def: 75,
+      spa: 105,
+      spd: 75,
+      spe: 75
+    }
+  },
+  genine: {
+    num: -42125,
+    name: "Genine",
+    types: ["Dark", "Ground"],
+    gender: "N",
+    baseStats: { hp: 90, atk: 100, def: 100, spa: 83, spd: 86, spe: 89 },
+    abilities: { 0: "Supreme Underlord", 1: "Bulletproof", H: "Quark Drive", S: "Good as Zinc" },
+    heightm: 6.3,
+    weightkg: 666.6,
+    color: "Black",
+    eggGroups: ["Smoggonpleasebankingambitsexual"],
+    gen: 8,
+    creator: "Mr. Sableye"
+  }
+};
+//# sourceMappingURL=pokedex.js.map
